b02072ad12377287fcebab6a60256670ac5375dd
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-09-22 16:04:38 +0200

    add checked exceptions InterruptOperationException and NotEnoughMoneyException; implement ConsoleHelper with basic IO methods

--- src/multiatm/ConsoleHelper.java
+++ src/multiatm/ConsoleHelper.java
@@ -1,4 +1,21 @@
 package multiatm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        try {
+            return bis.readLine();
+        } catch (IOException ignored) {
+        }
+        return null;
+    }
 }
---
+++ src/multiatm/exception/NotEnoughMoneyException.java
@@ -0,0 +1,4 @@
+package multiatm.exception;
+
+public class NotEnoughMoneyException extends Exception {
+}
